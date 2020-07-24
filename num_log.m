3bb6596afd2fca29f375ac2ceca44bf77c22a216
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-24 23:54:33 +0530

    returns the number of log singular Alpert rules.

---
+++ num_log.m
@@ -0,0 +1,27 @@
+function num = num_log ( )
+
+%*****************************************************************************80
+%
+%% NUM_LOG returns the number of Alpert rules for log singular functions.
+%
+%  Licensing:
+%
+%    This code is distributed under the GNU LGPL license.
+%
+%  Modified:
+%
+%    23 November 2015
+%
+%  Author:
+%
+%    John Burkardt
+%
+%  Parameters:
+%
+%    Output, integer NUM, the number of rules.
+%
+  num = 10;
+
+  return
+end
+
