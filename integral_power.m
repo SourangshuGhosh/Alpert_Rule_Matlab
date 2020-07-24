660f5cc6b5118785a23efe021bee32be888137f0
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-24 23:50:48 +0530

    returns the exact integral of the power singular test integrand.

---
+++ integral_power.m
@@ -0,0 +1,27 @@
+function value = integral_power ( )
+
+%*****************************************************************************80
+%
+%% INTEGRAL_POWER evaluates the test integral with power singularity.
+%
+%  Licensing:
+%
+%    This code is distributed under the GNU LGPL license.
+%
+%  Modified:
+%
+%    16 November 2015
+%
+%  Author:
+%
+%    John Burkardt
+%
+%  Parameters:
+%
+%    Output, real VALUE, the integral of the test integrand from 0 to 1.
+%
+  value = 0.079321002746971411182;
+
+  return
+end
+
