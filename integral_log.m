9ef2fdfd72286a772c81a001422ff8b443e5c111
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-24 23:49:41 +0530

    returns the exact integral of the log singular test integrand.

---
+++ integral_log.m
@@ -0,0 +1,31 @@
+function value = integral_log ( )
+
+%*****************************************************************************80
+%
+%% INTEGRAL_LOG evaluates the test integral with logarithmic singularity.
+%
+%  Licensing:
+%
+%    This code is distributed under the GNU LGPL license.
+%
+%  Modified:
+%
+%    15 November 2015
+%
+%  Author:
+%
+%    John Burkardt
+%
+%  Parameters:
+%
+%    Output, real VALUE, the integral of the test integrand from 0 to 1.
+%
+%
+%  1/200 (-Sin[3/10] + Sin[2003/10] - SinIntegral[200])
+%
+
+  value = -0.012771107587415899716;
+
+  return
+end
+
