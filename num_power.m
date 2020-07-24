85d7377ed22524e70a81fc6c4640eae9fc61b78c
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-24 23:54:47 +0530

    returns the number of power singular Alpert rules.

---
+++ num_power.m
@@ -0,0 +1,27 @@
+function num = num_power ( )
+
+%*****************************************************************************80
+%
+%% NUM_POWER returns the number of Alpert rules for power singular functions.
+%
+%  Licensing:
+%
+%    This code is distributed under the GNU LGPL license.
+%
+%  Modified:
+%
+%    23 November 2015
+%
+%  Author:
+%
+%    John Burkardt
+%
+%  Parameters:
+%
+%    Output, integer NUM, the number of rules.
+%
+  num = 12;
+
+  return
+end
+
